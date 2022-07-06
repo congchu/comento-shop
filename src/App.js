528208149e9fd15c7f6b0285430e623ab8f5abb1
KooMinJeong <cookie00421@gmail.com>
2022-07-06 17:19:24 +0900

    remove: App.css 파일 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Basket from "./pages/Basket";
-import "./App.css";
 
 function App() {
   return (
